ad59fb5957095f4ea66c5f48004d4d4540be30a1
Prakash Dhankecha <pdhankecha232@rku.ac.in>
2024-05-01 11:19:17 +0530

    scrape the data done

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,19 +1,55 @@
 "use client";
-import React from "react";
+import { scrapeAndStoreProduct } from "@/lib/actions";
+import React, { FormEvent, useState } from "react";
+
+const isValidAmazonProductURL = (url: string) => {
+    try {
+        const parsedUrl = new URL(url);
+        const hostname = parsedUrl.hostname;
+        
+        if (hostname.includes('amazon.com') || hostname.includes('amazon.') || hostname.includes('amazon')) {
+            return true;
+        }   
+    } catch (error) {
+        return false
+    }
+}
 
 const Searchbar = () => {
-  const handleSubmit = () => {};
+    
+    const [searchPrompt, setSearchPrompt] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
+    
+    if (!isValidLink) return alert(" Please provide a valid Amazon Product URL");
+        
+    try {
+        setIsLoading(true)
+        const product = await scrapeAndStoreProduct(searchPrompt)
+    } catch (error) {
+        console.log(error);
+        
+    } finally {
+        setIsLoading(false)
+    }
+  };
 
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
       <input
         type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
         placeholder="Enter Product Link"
         className="searchbar-input"
       />
 
-      <button type="submit" className="searchbar-btn">
-        Search
+      <button type="submit" className="searchbar-btn" disabled={searchPrompt === ''} >
+        {isLoading ? "Searching..." : "Search" }
       </button>
     </form>
   );
---
+++ lib/scraper/index.ts
@@ -0,0 +1,86 @@
+import axios from "axios";
+import cheerio from "cheerio";
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
+
+export async function scrapeAmazonProduct(url:string) {
+  if (!url) return;
+
+  // curl --proxy brd.superproxy.io:22225 --proxy-user brd-customer-hl_0ee6c56e-zone-costwise:7zy7d9r4u2zs -k "http://lumtest.com/myip.json"
+
+  // Bright Data Proxy configuration
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
+    const port = 22225;
+    const session_id = (1000000 * Math.random()) | 0;
+    const options = {
+        auth: {
+            username: `${username}-session-${session_id}`,
+            password,
+        },
+        host: 'brd.superproxy.io',
+        port,
+        rejectUnauthorized: false,
+    }
+
+    try {
+        // Fetch the product page
+        const response = await axios.get(url, options);
+        const $ = cheerio.load(response.data);
+
+        const title = $('#productTitle').text().trim();
+        const currentPrice = extractPrice(
+            $('.priceToPay span.a-price-whole'),
+            $('a.size.base.a-color-price'),
+            $('.a-button-selected .a-color-base'),
+            $('.a-price.a-text-price'),
+        );
+
+        const originalPrice = extractPrice(
+            $('#priceblock_ourprice'),
+            $('.a-price.a-text-price span.a-offscreen'),
+            $('#listPrice'),
+            $('#priceblock_dealprice'),
+            $('.a-size-base.a-color-price')
+        )
+
+        const outOfStock = $('#availability span').text().trim().toLowerCase().includes('currently unavailable');
+
+        const images = $('#imgBlkFront').attr('data-a-dynamic-image') || $('#landingImage').attr('data-a-dynamic-image');
+
+        const imageUrls = Object.keys(JSON.parse(images || '{}'));
+
+        const currency = extractCurrency($('.a-price-symbol'))
+
+        const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, '');
+
+        const discription = extractDescription($);
+
+        // console.log({title, currentPrice, originalPrice, outOfStock, imageUrls, currency, discountRate});
+        
+        // Construct the data object with scraped information
+        const data = {
+            url,
+            currency: currency || '$',
+            image: imageUrls[0],
+            title,
+            currentPrice: Number(currentPrice) || Number(originalPrice),
+            originalPrice: Number(originalPrice) || Number(currentPrice),
+            priceHistory: [],
+            discountRate: Number(discountRate),
+            category: 'category',
+            reviewCount: 100,
+            stars: 4.5,
+            isOutOfStock: outOfStock,
+            discription,
+            lowestPrice: Number(currentPrice) || Number(originalPrice),
+            highestPrice: Number(originalPrice) || Number(currentPrice),
+            average: Number(currentPrice) || Number(originalPrice),
+        }
+
+        // console.log(data);
+        return data;
+        
+    } catch (error: any) {
+        throw new Error(`Failed to scrape product: ${error.message}`);
+    }
+}
